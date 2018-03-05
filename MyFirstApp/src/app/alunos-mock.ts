cdc416e42854c40ee4ba5b8de404bc2622f62fe0
Rodrigo Luis de Faria <rodrigo.faria@inatel.br>
2018-03-05 19:20:50 -0300

    Create alunos-mock

---
+++ MyFirstApp/src/app/alunos-mock.ts
@@ -0,0 +1,15 @@
+import { Aluno } from "./aluno";
+
+export const ALUNOS : Aluno = [
+
+    {
+        nome : 'Rodrigo',
+        email : 'rodrigo@gmail.com',
+        cpf : '02344433382',
+        dataNascimento : new Date(),
+        peso : 34.64,
+        telefone : '34561190'
+    }
+
+
+];
